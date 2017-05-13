9b3bae89270b4e1b1eb80f88ac51aaa3c6638a87
Matt Larson <matt.a.larson@gmail.com>
2017-05-13 13:02:29 -0500

    basic DOM setup and controller wired

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -2,4 +2,14 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('MovieController', function($http) {
   console.log('NG');
+
+  var vm = this;
+
+  vm.getFilms = function() {
+    console.log('in getFilms');
+  };
+
+  vm.searchOmdb = function() {
+    console.log('button clicked');
+  };
 });
