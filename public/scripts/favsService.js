4e616959d7691bc423ec0130260270142b6bb978
Matt Larson <matt.a.larson@gmail.com>
2017-05-15 10:35:24 -0500

    added services folders

--- public/scripts/favsService.js
+++ public/scripts/favsService.js
@@ -1,15 +1,1 @@
-// // service to add favorites to MongoDB
-// myApp.service('addFavs', function($http) {
-//
-//   this.favToSend = {
-//     name: blah
-//   };
-//   //function to add a film to favorites listen
-//   this.$http({
-//     method: 'POST',
-//     url:'/',
-//     data: favToSend
-//   }).then(function(response){
-//     console.log(response);
-//   });
-// }); //end service function
+
